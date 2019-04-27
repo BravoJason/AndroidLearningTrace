60690315dc1926ca69a16dc748d6d6fcdbfadd9f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-27 14:56:14 +0000

    Deploy BravoJason/AndroidLearningTrace to github.com/BravoJason/AndroidLearningTrace.git:gh-pages

---
+++ app/src/main/java/android/learning/trace/android_learning_trace/presenter/IDateSetter.java
@@ -0,0 +1,6 @@
+package android.learning.trace.android_learning_trace.presenter;
+
+public interface IDateSetter {
+
+    void setDateValue(int y, int m, int d);
+}
--- app/src/main/java/android/learning/trace/android_learning_trace/view/BasicUIComponentP4Activity.java
+++ app/src/main/java/android/learning/trace/android_learning_trace/view/BasicUIComponentP4Activity.java
@@ -2,6 +2,7 @@ package android.learning.trace.android_learning_trace.view;
 
 
 import android.learning.trace.android_learning_trace.R;
+import android.learning.trace.android_learning_trace.presenter.IDateSetter;
 import android.learning.trace.android_learning_trace.presenter.ITimeSetter;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
@@ -9,20 +10,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class BasicUIComponentP4Activity extends AppCompatActivity implements ITimeSetter {
+public class BasicUIComponentP4Activity extends AppCompatActivity implements ITimeSetter, IDateSetter {
 
-    TextView tv_Time;
+    TextView tvTime, tvDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_uicomponent_p4);
         initTimePicker();
+        initDatePicker();
     }
 
     private void initTimePicker() {
         Button btnShowTimePickerDialog = findViewById(R.id.btn_p4_showTimePicker);
-        tv_Time = findViewById(R.id.tv_time);
+        tvTime = findViewById(R.id.tv_time);
         btnShowTimePickerDialog.setOnClickListener(v -> {
             DialogFragment timepickerFragment = new TimePickerFragment();
             timepickerFragment.show(getSupportFragmentManager(), "timePicker");
@@ -32,6 +34,24 @@ public class BasicUIComponentP4Activity extends AppCompatActivity implements ITi
 
     @Override
     public void setTimeValue(int nHour, int nMinute) {
-        tv_Time.setText("time:" + nHour + ":" + nMinute);
+        tvTime.setText("time:" + nHour + ":" + nMinute);
+    }
+
+    private void initDatePicker() {
+
+        tvDate = findViewById(R.id.tv_date);
+        Button btnShowDatePicker = findViewById(R.id.btn_p4_showDatePicker);
+        btnShowDatePicker.setOnClickListener(v -> {
+            DialogFragment dialogFragment = new DatePickerFragment();
+            dialogFragment.show(getSupportFragmentManager(), "datePicker");
+        });
+
+
+    }
+
+    @Override
+    public void setDateValue(int y, int m, int d) {
+
+        tvDate.setText(y + "-" + m + "-" + d);
     }
 }
