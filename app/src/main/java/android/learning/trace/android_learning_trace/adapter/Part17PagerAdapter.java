5ee01431ffacbacdd5d4f55855b8a5920e16bf96
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-01 17:02:33 +0000

    Deploy BravoJason/AndroidLearningTrace to github.com/BravoJason/AndroidLearningTrace.git:gh-pages

---
+++ app/src/main/java/android/learning/trace/android_learning_trace/adapter/Part17PagerAdapter.java
@@ -0,0 +1,93 @@
+package android.learning.trace.android_learning_trace.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class Part17PagerAdapter extends PagerAdapter {
+
+    private String[] titles = {"Dog1", "Dog2", "Dog3", "Dog4"};
+    private ArrayList<View> views;
+
+    public Part17PagerAdapter(ArrayList<View> views) {
+        this.views = views;
+
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return views.size();
+    }
+
+    /**
+     * Create the page for the given position.  The adapter is responsible
+     * for adding the view to the container given here, although it only
+     * must ensure this is done by the time it returns from
+     * {@link #finishUpdate(ViewGroup)}.
+     *
+     * @param container The containing View in which the page will be shown.
+     * @param position  The page position to be instantiated.
+     * @return Returns an Object representing the new page.  This does not
+     * need to be a View, but can be some other container of the page.
+     */
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View v = views.get(position);
+        container.addView(v);
+        return v;
+    }
+
+    /**
+     * Remove a page for the given position.  The adapter is responsible
+     * for removing the view from its container, although it only must ensure
+     * this is done by the time it returns from {@link #finishUpdate(ViewGroup)}.
+     *
+     * @param container The containing View from which the page will be removed.
+     * @param position  The page position to be removed.
+     * @param object    The same object that was returned by
+     *                  {@link #instantiateItem(View, int)}.
+     */
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView(views.get(position));
+    }
+
+    /**
+     * Determines whether a page View is associated with a specific key object
+     * as returned by {@link #instantiateItem(ViewGroup, int)}. This method is
+     * required for a PagerAdapter to function properly.
+     *
+     * @param view   Page View to check for association with <code>object</code>
+     * @param object Object to check for association with <code>view</code>
+     * @return true if <code>view</code> is associated with the key object <code>object</code>
+     */
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+
+        return view == object;
+    }
+
+    /**
+     * This method may be called by the ViewPager to obtain a title string
+     * to describe the specified page. This method may return null
+     * indicating no title for this page. The default implementation returns
+     * null.
+     *
+     * @param position The position of the title requested
+     * @return A title for the requested page
+     */
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        return titles[position];
+    }
+}
