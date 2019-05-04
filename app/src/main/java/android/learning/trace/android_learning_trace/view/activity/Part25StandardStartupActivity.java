0dd9fc6091212ca162b11136bbf89327af033537
BravoJason <zerone.jia@gmail.com>
2019-05-04 14:22:55 -0400

    Finish Intent learning.
    Signed-off-by: BravoJason <zerone.jia@gmail.com>

---
+++ app/src/main/java/android/learning/trace/android_learning_trace/view/activity/Part25StandardStartupActivity.java
@@ -0,0 +1,27 @@
+package android.learning.trace.android_learning_trace.view.activity;
+
+import android.content.Intent;
+import android.learning.trace.android_learning_trace.R;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class Part25StandardStartupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part25_standard_startup);
+    }
+
+    public void StartActivityStandMode(View view) {
+        Intent intent = new Intent(this, Part25StandardStartupActivity.class);
+        startActivity(intent);
+    }
+
+    public void StartPart25IntentActivity(View view) {
+
+        Intent intent = new Intent(this, Part25IntentActivity.class);
+        startActivity(intent);
+    }
+}
